7942b0a6131100013c77aef5a82d119727b7a71d
Sanjay <treeinrandomforest@gmail.com>
2025-04-22 13:55:16 -0400

    scripts to create csv files

---
+++ scripts/gen_csv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ueo pipefail
+
+LOC=$1 #location of log files
+
+OUTLOC=/home/sanjay/experiments/MOC_gpt2_training_results/data
+OUTFILE=`echo $LOC | awk -F"/" '{print $NF}'`".txt"
+echo $OUTLOC/$OUTFILE
+
+grep step -a $LOC/stdout* | grep "/10" | grep -v "1/10" | grep -v "2/10" | awk '{print $1, $14}' | sed s:"("::g > $OUTLOC/$OUTFILE
+#if [ `wc -l $OUT_LOC/OUTFILE` != 80 ]
+#then
+#	echo "NOT EQUAL"
+#fi
+
+CSV_OUTFILE=`echo $OUTFILE | sed s:".txt":".csv":g`
+echo $OUTLOC/$CSV_OUTFILE
+awk -f process_grep.awk $OUTLOC/$OUTFILE > $OUTLOC/$CSV_OUTFILE
