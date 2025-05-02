57f9b41489c60ae7fe092dc661386c516edcfbbe
Jonathan Appavoo <jappavoo@bu.edu>
2025-05-02 11:30:52 -0400

    filtering so only data lines are included and reverse (worst) support

--- misc/ociperf_data/best.sh
+++ misc/ociperf_data/best.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 typeset -r SEP=','
 typeset -r FIELD=${BW_FIELD:-14}
+FLG=""
+
+[[ $1 =~ ^\- ]] && {
+   FLG=$1
+   shift
+}	
+   
 typeset -r -i num=$1
 shift
 
 [[ -z $num || $num = 0 ]] && {
-   echo "USAGE: $0 <num> <csv files>" > /dev/stderr
+   echo "USAGE: $0 [-r] <num> <csv files>" > /dev/stderr
    echo "  assumes bw is field 14 and prints num best bandwidths" > /dev/stderr
+   echo "-r will reverse the sort and give you the worst values" 
    exit -1
-}	
- 
-sort -n -t${SEP} -k ${FIELD} $@  | tail -${num}
+}
+
+cat $@ | grep '.*/sec$' | sort -n $FLG -t${SEP} -k ${FIELD} | tail -${num}
