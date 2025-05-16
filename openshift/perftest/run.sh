b319c1557581b8e8650bbd3e34da9909d5cf09d5
EldritchJS <jschlessman@gmail.com>
2025-05-16 19:41:51 -0400

    Fixed to account for case with 1 GPU

--- openshift/perftest/run.sh
+++ openshift/perftest/run.sh
@@ -9,6 +9,8 @@ HCA="mlx5_2"
 FLAGS="-a -R -T 41 -F -x 3 -m 4096 --report_gbits -d ${HCA} -p 10000"
 MTU=9000
 BM_HOSTS="sriovlegacy-workload-uno sriovlegacy-workload-dos"
+NUM_GPU=0
+DRYRUN=0
 
 function log()
 {
@@ -16,8 +18,6 @@ function log()
 }
 
 typeset -a  hosts=()
-
-dryrun=0
 
 hexec="oc exec"
 
@@ -39,14 +39,14 @@ for srv in ${hosts[@]}; do
 				LOGFILE="${LOGDIR}/perftest_cpu_srv_${TEST}_${MTU}_${QP}_${srvnode}_${cltnode}.log"
 				COMMAND="$hexec ${srv} -- ${TEST} ${FLAGS} -q ${QP} 2>&1 > ${LOGFILE} &" 
 				log "${COMMAND}"
-				if [ "$dryrun" -eq 0 ]; then
+				if [ "$DRYRUN" -eq 0 ]; then
 					eval "${COMMAND}" 
 					sleep 1
 				fi
 				LOGFILE="${LOGDIR}/perftest_cpu_clt_${TEST}_${MTU}_${QP}_${srvnode}_${cltnode}.log"
 				COMMAND="$hexec ${clt} -- ${TEST} ${FLAGS} -q ${QP} ${srvip} 2>&1 > ${LOGFILE}"
 				log "${COMMAND}"
-				if [ "$dryrun" -eq 0 ]; then
+				if [ "$DRYRUN" -eq 0 ]; then
 					eval "${COMMAND}" 
 					sleep 1
 				fi
@@ -65,22 +65,23 @@ for srv in ${hosts[@]}; do
 	for clt in ${hosts[@]}; do
         [[ $srv == $clt ]] && continue
 		cltnode=`oc get pod ${clt} -o wide | tail -n1 | tr -s ' ' | cut -d' ' -f7`
-		for GPU_S in {0..3}; do
-			for GPU_C in {0..3}; do
+		for ((GPU_S = 0; GPU_S <= $NUM_GPU; GPU_S++)); do #in {0..$NUM_GPU}; do
+			for ((GPU_C = 0; GPU_C <= $NUM_GPU; GPU_C++)); do #in {0..$NUM_GPU}; do
+			#for GPU_C in {0..$NUM_GPU}; do
 				for TEST in 'ib_write_bw' 'ib_read_bw'; do
 					for QP in 1 2 4 8 16 32; do
 						log "$TEST : Node ${srvnode} (Pod ${srv} - Interface IP ${srvip}) [${GPU_S}] <-> [${GPU_C}] Node ${cltnode} (Pod ${clt})"
 						LOGFILE="${LOGDIR}/perftest_gpu_srv_${TEST}_${MTU}_${QP}_${srvnode}_${cltnode}_${GPU_S}_${GPU_C}.log"
 						COMMAND="$hexec ${srv} -- ${TEST} ${FLAGS} -q ${QP} --use_cuda=$GPU_S --use_cuda_dmabuf 2>&1 > ${LOGFILE} &"
 						log "${COMMAND}"
-						if [ "$dryrun" -eq 0 ]; then
+						if [ "$DRYRUN" -eq 0 ]; then
 							eval "${COMMAND}" 
 							sleep 1
 						fi
 						LOGFILE="${LOGDIR}/perftest_gpu_clt_${TEST}_${MTU}_${QP}_${srvnode}_${cltnode}_${GPU_S}_${GPU_C}.log"
 						COMMAND="$hexec ${srv} -- ${TEST} ${FLAGS} -q ${QP} --use_cuda=$GPU_C --use_cuda_dmabuf ${srvip} 2>&1 > ${LOGFILE}"
 						log "${COMMAND}"
-						if [ "$dryrun" -eq 0 ]; then
+						if [ "$DRYRUN" -eq 0 ]; then
 							eval "${COMMAND}" 
 							sleep 1
 						fi
