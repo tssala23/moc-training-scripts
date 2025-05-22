96416aa0cd392c9edaa98811da97ba6126e9da05
Sanjay <treeinrandomforest@gmail.com>
2025-05-22 12:27:12 -0400

    print string

--- testing-tcp-rdma-gdrdma/bm-oc-comparison/get_data.sh
+++ testing-tcp-rdma-gdrdma/bm-oc-comparison/get_data.sh
@@ -20,7 +20,11 @@ fi
 ./gen_csv.sh 'logs_oc/may19protoscanhostnetwork/multipod*profile1*' TEMP_oc $OUT_LOC/oc_may19_profile1.csv 1
 ./gen_csv.sh 'logs_oc/may21protoscanhostnetworkncclfix/multipod*profile0*' TEMP_oc $OUT_LOC/oc_may21_profile0.csv 1
 ./gen_csv.sh 'logs_oc/may21protoscanhostnetworkncclfix/multipod*profile1*' TEMP_oc $OUT_LOC/oc_may21_profile1.csv 1
-./gen_csv.sh 'logs_bm_noprofile/*' TEMP_bm bm_noprofile.csv 0
-./gen_csv.sh 'logs_bm_profile/*' TEMP_bm bm_profile.csv 0 log
+./gen_csv.sh 'logs_bm_noprofile/*' TEMP_bm $OUT_LOC/bm_noprofile.csv 0
+./gen_csv.sh 'logs_bm_profile/*' TEMP_bm $OUT_LOC/bm_profile.csv 0 log
 
 rm TEMP_*
+
+set +x
+ls csvs/ | paste | while read line; do echo -n $line","; done; echo
+
