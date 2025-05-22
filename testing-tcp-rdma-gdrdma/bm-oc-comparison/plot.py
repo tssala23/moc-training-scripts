802ed3dbbdef7e6339405bcecfee6d53276f6678
Sanjay <treeinrandomforest@gmail.com>
2025-05-22 13:27:22 -0400

    updating plotting scrpit

--- testing-tcp-rdma-gdrdma/bm-oc-comparison/plot.py
+++ testing-tcp-rdma-gdrdma/bm-oc-comparison/plot.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import plotly.express as px
+import plotly.graph_objects as gp
 
 df_bm_np = pd.read_csv("csvs/bm_noprofile.csv")
 df_bm_np['profile'] = 0
@@ -22,4 +24,25 @@ df_oc['env'] = 'oc'
 df = pd.concat([df_bm, df_oc], axis=0)
 df['type'] = df['type'].str.lower()
 
+#remove tcp results on oc where NCCL_NTHREADS_PER_SOCKET and NCCL_SOCKS_PER_THREAD are not set
+#generally use 16 and 4 for these params to be consistent with bare-metal
+df = df[~((df['run_id']=='may19protoscanhostnetwork') & (df['type']=='tcp'))].copy() 
+
 df.to_csv("csvs/agg.csv", index=False)
+
+df_agg = df.groupby(["profile", "type", "env"]).agg({'time': ["mean", "std"]}).reset_index()
+df_agg.columns = ['profile', 'type', 'env', 'time_mean', 'time_std']
+
+df_agg["time_mean_label"] = df_agg["time_mean"].apply(lambda x: f'{x:.2f}ms')
+
+fig = px.bar(df_agg[df_agg.profile==0], 
+        x="type", 
+        y="time_mean", 
+        color="env", 
+        barmode="group", 
+        text="time_mean_label",
+        error_y="time_std",
+        log_y=False)
+fig.update_traces(textfont_size=24)
+fig.update_layout(xaxis={'categoryorder': 'array', 
+                         'categoryarray': ["tcp", "rdma", "gdr"]})
