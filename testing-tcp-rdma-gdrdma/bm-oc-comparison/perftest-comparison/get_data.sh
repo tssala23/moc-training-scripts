513a504cd302454159abcb537d4aa034ac498d16
Sanjay <treeinrandomforest@gmail.com>
2025-05-23 16:30:25 -0400

    adding perftest plots

--- testing-tcp-rdma-gdrdma/bm-oc-comparison/perftest-comparison/get_data.sh
+++ testing-tcp-rdma-gdrdma/bm-oc-comparison/perftest-comparison/get_data.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-ln -s ../../../baremetal/perftest/perftest_cpu.csv bm_perftest_cpu.csv
-ln -s ../../../baremetal/perftest/perftest_gpu.csv bm_perftest_gpu.csv
+if [ ! -f bm_perftest_cpu.csv ]
+then
+	ln -s ../../../baremetal/perftest/perftest_cpu.csv bm_perftest_cpu.csv
+fi
 
+if [ ! -f bm_perftest_gpu.csv ]
+then
+	ln -s ../../../baremetal/perftest/perftest_gpu.csv bm_perftest_gpu.csv
+fi
+
+if [ ! -f oc_perftest_cpu.csv ]
+then
+	ln -s ../../../openshift/perftest/perftest_cpu.csv oc_perftest_cpu.csv
+fi
+
+if [ ! -f oc_perftest_gpu.csv ]
+then
+	ln -s ../../../openshift/perftest/perftest_gpu.csv oc_perftest_gpu.csv
+fi
---
+++ testing-tcp-rdma-gdrdma/bm-oc-comparison/perftest-comparison/plot.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import plotly.express as px
+import numpy as np
+
+#data prep
+df_bm_cpu = pd.read_csv("bm_perftest_cpu.csv")
+df_bm_cpu["type"] = "cpu"
+df_bm_cpu["env"] = "bm"
+
+df_bm_gpu = pd.read_csv("bm_perftest_gpu.csv")
+df_bm_gpu["type"] = "gpu"
+df_bm_gpu["env"] = "bm"
+
+df_oc_cpu = pd.read_csv("oc_perftest_cpu.csv")
+df_oc_cpu["type"] = "cpu"
+df_oc_cpu["env"] = "oc"
+
+df_oc_gpu = pd.read_csv("oc_perftest_gpu.csv")
+df_oc_gpu["type"] = "gpu"
+df_oc_gpu["env"] = "oc"
+
+df_cpu = pd.concat([df_bm_cpu, df_oc_cpu], axis=0)
+df_gpu = pd.concat([df_bm_gpu, df_oc_gpu], axis=0)
+
+#flatten data
+def flatten(df_cpu, qp=2, test="ib_read_bw"): #using this var name so don't have to change func def
+    id_vars = ["test", "qp", "metric", "type", "env"]
+    df_cpu_flat = pd.melt(df_cpu, 
+                      value_vars=df_cpu.columns[6:29],
+                      var_name="size", 
+                      value_name="bw", 
+                      id_vars=id_vars)
+
+    df_cpu_filtered = df_cpu_flat[(df_cpu_flat.qp==qp) & (df_cpu_flat.metric=="avg") & (df_cpu_flat.test==test)]
+    df_cpu_grouped = df_cpu_filtered.groupby(id_vars + ["size"]).agg({"bw": {"mean", "std"}}).reset_index()
+
+    colnames = ["test", "qp", "metric", "type", "env", "size", "bw_mean", "bw_std"]
+    df_cpu_grouped.columns = colnames
+
+    return df_cpu_flat, df_cpu_grouped, df_cpu_filtered.groupby("env").max()["bw"].to_dict(), df_cpu_filtered
+
+def plot(df_cpu_grouped, title=None, subtitle=None):
+    fig = px.bar(df_cpu_grouped, x="size", y="bw_mean", error_y="bw_std", color="env", barmode="group", title=title, subtitle=subtitle)
+
+    fig.update_layout(xaxis={'categoryorder': 'array',
+                             'categoryarray': np.sort(df_cpu_grouped["size"].unique().astype(int)).astype(str)})
+    return fig
+
+#CPU plot
+def plot_full(df_cpu, qp=2, test="ib_read_bw", outfile="plots/cpu_rdma_qp2_write.png"):
+    df_cpu_flat, df_cpu_grouped, maxbw, df_cpu_filtered = flatten(df_cpu, qp=qp, test=test)
+
+    fig = px.bar(df_cpu_grouped, x="size", 
+                 y="bw_mean", 
+                 error_y="bw_std", 
+                 color="env", 
+                 barmode="group", 
+                 title=f"RDMA {test} (#queue-pairs={qp}) Perftest Host-to-Host Bandwidth Measurements",
+                 subtitle=f"Max Bare-metal BW = {maxbw['bm']}Gbps - Max OpenShift BW = {maxbw['oc']}Gbps")
+
+    fig.update_layout(xaxis={'categoryorder': 'array',
+                             'categoryarray': np.sort(df_cpu_grouped["size"].unique().astype(int)).astype(str)})
+
+
+    fig.update_layout(
+        xaxis_title="Size (Bytes)",
+        yaxis_title="Mean Bandwidth (Gbps)"
+        )
+
+    fig.write_image(outfile, height=800, width=1200)
+    
+    return fig
+
+for qp in [2, 4, 8, 16, 32]:
+    for test in ["ib_read_bw", "ib_write_bw"]:
+        fig = plot_full(df_cpu, qp=qp, test=test, outfile=f"plots/cpu_rdma_qp{qp}_{test}.png")
+
