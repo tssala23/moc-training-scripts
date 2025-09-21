6fb4cf89788ae7172d6f54320c46f515adcdab86
EldritchJS <jschlessman@gmail.com>
2025-09-21 16:35:38 -0400

    Rename pciacs script

---
+++ yaml/rdma/pciacs_set.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: ${1} <enable|disable>"
+fi
+
+if [[ "$OPERATION" == "disable" ]]; then
+
+  for BDF in `lspci -d "*:*:*" | awk '{print $1}'`; do
+	setpci -v -s ${BDF} ECAP_ACS+0x6.w > /dev/null 2>&1
+	if [ $? -ne 0 ]; then
+		continue
+	fi
+	echo -n "Disabling for Device $BDF: "
+	setpci -v -s ${BDF} ECAP_ACS+0x6.w=0000
+	setpci -v -s ${BDF} ECAP_ACS+0x6.w
+  done
+
+elif [[ "$OPERATION" == "enable" ]]; then
+
+  for BDF in `lspci -d "*:*:*" | awk '{print $1}'`; do
+        setpci -v -s ${BDF} ECAP_ACS+0x6.w > /dev/null 2>&1
+        if [ $? -ne 0 ]; then
+                continue
+        fi
+        echo -n "Disabling for Device $BDF: "
+        setpci -v -s ${BDF} ECAP_ACS+0x6.w=0000
+        setpci -v -s ${BDF} ECAP_ACS+0x6.w
+  done
+
+else
+  echo "Unknown option ${OPERATION}"
+  exit 1
+fi
