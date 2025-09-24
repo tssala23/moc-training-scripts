a883adb7d53bddaefcaad3b76264ff8069f97dcf
EldritchJS <jschlessman@gmail.com>
2025-09-24 11:39:54 -0400

    Added permanent disable of 001d for acs

--- yaml/rdma/pciacs_set.sh
+++ yaml/rdma/pciacs_set.sh
@@ -4,27 +4,25 @@ if [ "$#" -ne 1 ]; then
   echo "Usage: ${1} <enable|disable>"
 fi
 
-if [[ "$OPERATION" == "disable" ]]; then
+if [[ "$OPERATION" == "enable" ]]; then
 
   for BDF in `lspci -d "*:*:*" | awk '{print $1}'`; do
 	setpci -v -s ${BDF} ECAP_ACS+0x6.w > /dev/null 2>&1
 	if [ $? -ne 0 ]; then
 		continue
 	fi
-	echo -n "Disabling for Device $BDF: "
 	setpci -v -s ${BDF} ECAP_ACS+0x6.w=0000
 	setpci -v -s ${BDF} ECAP_ACS+0x6.w
   done
 
-elif [[ "$OPERATION" == "enable" ]]; then
+elif [[ "$OPERATION" == "disable" ]]; then
 
   for BDF in `lspci -d "*:*:*" | awk '{print $1}'`; do
         setpci -v -s ${BDF} ECAP_ACS+0x6.w > /dev/null 2>&1
         if [ $? -ne 0 ]; then
                 continue
         fi
-        echo -n "Disabling for Device $BDF: "
-        setpci -v -s ${BDF} ECAP_ACS+0x6.w=0000
+        setpci -v -s ${BDF} ECAP_ACS+0x6.w=001d
         setpci -v -s ${BDF} ECAP_ACS+0x6.w
   done
 
