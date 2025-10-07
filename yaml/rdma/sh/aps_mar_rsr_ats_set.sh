ef80d0fa8e3a484a6ca3a99e564a9801ba826adf
EldritchJS <jschlessman@gmail.com>
2025-10-07 10:47:49 -0400

    Made mellanox driver settings for all devices

--- yaml/rdma/sh/aps_mar_rsr_ats_set.sh
+++ yaml/rdma/sh/aps_mar_rsr_ats_set.sh
@@ -8,32 +8,16 @@ fi
 
 OPERATION=$1
 
+for d in $(mlxconfig query |grep Device:|awk '{print $2}'); do
+  if [[ "$OPERATION" == "enable" ]]; then
+    mlxconfig -y -d $d s ADVANCED_PCI_SETTINGS=1
+    mlxconfig -y -d $d s MAX_ACC_OUT_READ=128 RDMA_SELECTIVE_REPEAT_EN=1 ATS_ENABLED=1
+  elif [[ "$OPERATION" == "disable" ]]; then
+    mlxconfig -y -d $d s MAX_ACC_OUT_READ=0 RDMA_SELECTIVE_REPEAT_EN=0 ATS_ENABLED=0
+    mlxconfig -y -d $d s ADVANCED_PCI_SETTINGS=0
+  else
+    echo "Unknown option ${OPERATION}"
+    exit 1
+  fi
+done
 
-if [[ "$OPERATION" == "enable" ]]; then
-  mlxconfig -y -d 0000:03:00.0 s ADVANCED_PCI_SETTINGS=1
-  mlxconfig -y -d 0000:03:00.0 s MAX_ACC_OUT_READ=128 RDMA_SELECTIVE_REPEAT_EN=1 ATS_ENABLED=1
-
-  mlxconfig -y -d 0000:c3:00.0 s ADVANCED_PCI_SETTINGS=1
-  mlxconfig -y -d 0000:c3:00.0 s MAX_ACC_OUT_READ=128 RDMA_SELECTIVE_REPEAT_EN=1 ATS_ENABLED=1
-
-  mlxconfig -y -d 0000:a3:00.0 s ADVANCED_PCI_SETTINGS=1
-  mlxconfig -y -d 0000:a3:00.0 s MAX_ACC_OUT_READ=128 RDMA_SELECTIVE_REPEAT_EN=1 ATS_ENABLED=1
-
-  mlxconfig -y -d 0000:23:00.0 s ADVANCED_PCI_SETTINGS=1
-  mlxconfig -y -d 0000:23:00.0 s MAX_ACC_OUT_READ=128 RDMA_SELECTIVE_REPEAT_EN=1 ATS_ENABLED=1
-elif [[ "$OPERATION" == "disable" ]]; then
-  mlxconfig -y -d 0000:03:00.0 s MAX_ACC_OUT_READ=0 RDMA_SELECTIVE_REPEAT_EN=0 ATS_ENABLED=0
-  mlxconfig -y -d 0000:03:00.0 s ADVANCED_PCI_SETTINGS=0
-
-  mlxconfig -y -d 0000:c3:00.0 s MAX_ACC_OUT_READ=0 RDMA_SELECTIVE_REPEAT_EN=0 ATS_ENABLED=0
-  mlxconfig -y -d 0000:c3:00.0 s ADVANCED_PCI_SETTINGS=0
-
-  mlxconfig -y -d 0000:a3:00.0 s MAX_ACC_OUT_READ=0 RDMA_SELECTIVE_REPEAT_EN=0 ATS_ENABLED=0
-  mlxconfig -y -d 0000:a3:00.0 s ADVANCED_PCI_SETTINGS=0
-
-  mlxconfig -y -d 0000:23:00.0 s MAX_ACC_OUT_READ=0 RDMA_SELECTIVE_REPEAT_EN=0 ATS_ENABLED=0
-  mlxconfig -y -d 0000:a3:00.0 s ADVANCED_PCI_SETTINGS=0
-else
-  echo "Unknown option ${OPERATION}"
-  exit 1
-fi
